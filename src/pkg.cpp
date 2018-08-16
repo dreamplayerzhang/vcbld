d49abe52fea13a37e8c357f249a1bde609e9dbbe
Mohammed Alyousef <may642_2000@hotmail.com>
2018-08-16 13:23:48 +0200

    removed unnecessary classes

--- src/pkg.cpp
+++
@@ -1,8 +0,0 @@
-#include "pkg.h"
-
-namespace vcbld
-{
-PkgClass::PkgClass(const std::string &pkg) {}
-std::string PkgClass::packageName() const {}
-std::string PkgClass::version() const {}
-} // namespace vcbld

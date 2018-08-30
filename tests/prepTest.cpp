40903139a90c5d6112dfa62be4c31ec7033ade8f
Mohammed Alyousef <may642_2000@hotmail.com>
2018-08-30 15:48:37 +0200

    typo

--- tests/prepTest.cpp
+++ tests/prepTest.cpp
@@ -11,7 +11,7 @@ TEST_CASE("Preparation tests", "[prep]") {
   CHECK(prepClass.stripLibName("vcbldlib.lib") == "vcbldlib");
   CHECK(prepClass.sourceFilesSinPath() == "main.cpp ");
   std::string path = (fs::canonical(prepClass.projPath()) / "src" / "main.cpp").string();
-  preClass.posixify(path);
+  prepClass.posixify(path);
   CHECK(
       prepClass.sourceFiles() ==
       "\"" +

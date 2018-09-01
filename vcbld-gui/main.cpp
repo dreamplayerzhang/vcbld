adc09978443076100df3e053baace367332121ae
Mohammed Alyousef <may642_2000@hotmail.com>
2018-09-02 00:15:04 +0200

    show console w gui on windows

--- vcbld-gui/main.cpp
+++ vcbld-gui/main.cpp
@@ -5,11 +5,16 @@ using namespace vcbld;
 int main(int argc, char *argv[]) {
   fs::path vcbldPath = argv[0];
   vcbldPath = vcbldPath.parent_path();
-  
+
 #if defined(__APPLE__) && defined(__MACH__)
   if (vcbldPath.filename().string() == "MacOS") {
     vcbldPath = vcbldPath.parent_path().parent_path().parent_path();
   }
+#endif
+
+#if defined(Q_OS_WIN)
+#include <windows.h>
+  ::ShowWindow(::GetConsoleWindow(), SW_SHOW);
 #endif
 
   QApplication a(argc, argv);

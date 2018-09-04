87e0fb9e083f601fcb8ae498e005a98e23ac25c5
MoAlyousef <37966791+MoAlyousef@users.noreply.github.com>
2018-09-04 02:52:32 +0200

    Update remove.cpp

--- vcbld-gui/remove.cpp
+++ vcbld-gui/remove.cpp
@@ -31,9 +31,4 @@ void Remove::on_listWidget_itemDoubleClicked(QListWidgetItem *item) {
   _output = Helper::execVec(std::bind(&args::remove, v), v);
   emit outputChanged(_output);
   item->setHidden(true);
-//  ui->listWidget->clear();
-//  for (std::vector<std::string>::iterator it = pkgClass.packageNames().begin();
-//       it != pkgClass.packageNames().end(); ++it) {
-//    ui->listWidget->addItem(QString::fromStdString(*it));
-//  }
 }
